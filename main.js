56ad2f36d5d59bf71413ff68fe65f56ceb7d92fa
UnyieldingTrilobite <72010865+UnyieldingTrilobite@users.noreply.github.com>
2020-11-14 19:20:29 +0800

    Create main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var yulu_all=[
-'聪明的老师生气了，想怎么样怎么样',
-'我就是要你们每天早上起来到实验室去吃点铁钉'
-];
-var rand_yulu=yulu_all[Math.floor(Math.random()*yulu_all.length)];
-console.log(rand_yulu);
+        var yulu_all=[
+        '聪明的老师生气了，想怎么样怎么样',
+        '我就是要你们每天早上起来到实验室去吃点铁钉'
+        ];
+        var rand_yulu=yulu_all[Math.floor(Math.random()*yulu_all.length)];
+        document.write(rand_yulu);
